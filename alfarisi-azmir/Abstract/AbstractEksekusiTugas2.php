de2b203ada494803f389e2f4d745876de11df73e
farishhz <alfarisiazmir14@gmail.com>
2026-07-28 16:11:25 +0700

    faris suka mira

--- alfarisi-azmir/Abstract/AbstractEksekusiTugas2.php
+++ alfarisi-azmir/Abstract/AbstractEksekusiTugas2.php
@@ -4,7 +4,7 @@ require_once 'AbstractPembayaran.php';
 require_once 'AbstractTransferBank.php';
 require_once 'AbstractEWallet.php';
 
-echo "<center><h1>Riwayat Transaksi</h1></center>";
+echo "<center><h1>Riwayat Transaksimu</h1></center>";
 
 echo "<hr><h3>Transaksi 1:</h3>";
 $transaksiBank = new TransferBank(800000, "676-767-6767");
